e48ade39525cbba3d3c835d1c811927a0955fef0
yangeng2 <yangeng2@f403994e-9d58-42ba-894d-10d84a558d0e>
2014-09-05 21:16:58 +0000

    lab_debug submission

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -17,7 +17,8 @@ RGBAPixel * myFavoriteColor(int intensity)
 	color.red   = 0;
 	color.green = intensity/2;
 	color.blue  = intensity;
-	return &color;
+	RGBAPixel * c = new RGBAPixel(color);
+	return c;
 }
 
 
@@ -26,22 +27,25 @@ void sketchify()
 	// Load in.png
 	PNG * original;
 
+	//std::cout << "Reached line 28" << endl;	
+	original = new PNG();
 	original->readFromFile("in.png");
 	int width  = original->width();
 	int height = original->height();
+	//std::cout << "Reached line 32" << endl;
 
 	// Create out.png
-	PNG * output;
-	setupOutput(width, height);	
+	PNG * output = setupOutput(width, height);	
 
 	// Loud our favorite color to color the outline
 	RGBAPixel * myPixel = myFavoriteColor(192);
 
 	// Go over the whole image, and if a pixel differs from that to its upper
 	// left, color it my favorite color in the output
-	for (int y = 1; 0 < y < height; y++)
+	for (int y = 1; y < height; y++)
 	{
-		for (int x = 1; 0 < x < width; x++)
+		//std::cout << "begin" << endl;
+		for (int x = 1; x < width; x++)
 		{
 			// Calculate the pixel difference
 			RGBAPixel * prev = (*original)(x-1, y-1);
@@ -52,17 +56,24 @@ void sketchify()
 
 			// If the pixel is an edge pixel,
 			// color the output pixel with my favorite color
-			RGBAPixel * currOutPixel = (*output)(x,y);
+			RGBAPixel * currOutPixel =(*output)(x,y);
+
 			if (diff > 100)
-				currOutPixel = myPixel;
+				*currOutPixel = *myPixel;
+			
 		}
+		//std::cout << "end" << endl;
 	}
 	
 	// Save the output file
+	std::cout << "begin" << endl;
 	output->writeToFile("out.png");
+	std::cout << "end" << endl;
 
 	// Clean up memory
-	delete myPixel;
+	//delete myPixel;
+	std::cout << "check1" << endl;
 	delete output;
 	delete original;
+	std::cout << "check2" << endl;
 }
